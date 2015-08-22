2e60cd9a100cc1f86dd75f499cfc4dfa689e7334
to-more <to_more@mailoo.org>
2015-08-22 01:51:01 -0300

    add the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,13 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
